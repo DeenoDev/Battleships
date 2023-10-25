a779547482a100dbcd77c0f723271a5892e2265f
DeenoDev <deyanoklaasen@outlook.com>
2023-10-25 23:28:28 +0200

    Started scripting flip function to flip ships.

---
+++ main.js
@@ -0,0 +1,9 @@
+const optionContainer = document.querySelector('.option-container');
+const flipButton = document.querySelector('#flip-button');
+
+function flip(){
+    const optionShips = (Array.from (optionContainer.children));
+}
+
+
+flipButton.addEventListener('click', flip);
