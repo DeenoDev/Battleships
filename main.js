b7b93704de0f4b9e5290c76b1834febfb3a38e5b
DeenoDev <deyanoklaasen@outlook.com>
2023-10-25 23:36:40 +0200

    flip function completed.

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 const optionContainer = document.querySelector('.option-container');
 const flipButton = document.querySelector('#flip-button');
 
+
+let angle = 0;
 function flip(){
-    const optionShips = (Array.from (optionContainer.children));
-    optionShips.forEach(optionShip => optionShip.style.transform = 'rotate(90deg)');
+    const optionShips = Array.from (optionContainer.children);
+    if(angle === 0) {
+        angle = 90;
+    } else {
+        angle = 0;
+    }
+    optionShips.forEach(optionShip => optionShip.style.transform = `rotate(${angle}deg)`);
 }
 
 
