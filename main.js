7b124674040a4d4984f3e13a4e3c35e6221ac698
DeenoDev <deyanoklaasen@outlook.com>
2023-10-25 23:43:05 +0200

    createBoard function declared.

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const optionContainer = document.querySelector('.option-container');
 const flipButton = document.querySelector('#flip-button');
 
-
+//Option Choosing
 let angle = 0;
 function flip(){
     const optionShips = Array.from (optionContainer.children);
@@ -10,7 +10,16 @@ function flip(){
     } else {
         angle = 0;
     }
+    //    Turnarury Function
+    // angle = angle === 0 ? 90 : 0;
     optionShips.forEach(optionShip => optionShip.style.transform = `rotate(${angle}deg)`);
+}
+
+//Creating Boards
+const width = 10;
+
+function createBoard(){
+    
 }
 
 
