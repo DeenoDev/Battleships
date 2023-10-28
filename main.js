4b925d4b3fa806216037b67733a2c8aa49618cea
DeenoDev <deyanoklaasen@outlook.com>
2023-10-28 15:09:18 +0200

    declared addShip function that adds ship to gameboard for computer.

--- main.js
+++ main.js
@@ -57,5 +57,11 @@ const carrier = new Ship('carrier', 5);
 
 const ships = [destroyer, submarine, cruiser, battleship, carrier];
 
+function addShipPiece(){
+   const allBoardBlocks = document.querySelectorAll('#computer div');
+   console.log(allBoardBlocks);
+}
+addShipPiece();
+
 
 
